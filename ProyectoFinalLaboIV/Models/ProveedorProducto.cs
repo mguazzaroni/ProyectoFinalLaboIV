8cfa9b594eebedafd13a0c83733831bee0354de4
mguazzaroni <matiasguazzaroni10@gmail.com>
2021-12-09 17:18:58 -0300

    Fix Proveedor-Producto

--- ProyectoFinalLaboIV/Models/ProveedorProducto.cs
+++ ProyectoFinalLaboIV/Models/ProveedorProducto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,9 @@ namespace ProyectoFinalLaboIV.Models
 {
     public class ProveedorProducto
     {
+        [Display(Name="Proveedor")]
         public int ProveedorId { get; set; }
+        [Display(Name = "Producto")]
         public int ProductoId { get; set; }
         public Proveedor Proveedor{ get; set; }
         public Producto Producto { get; set; }
